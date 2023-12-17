b3cf8f6c2573da5f1152d5b0c46d84322112652b
rajanshrestha09 <139428256+rajanshrestha09@users.noreply.github.com>
2023-12-17 19:20:24 +0000

    basic variable

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,14 @@
+// Important (const, let, var: var use less or no use)
+const accountId = 213654;
+let accountEmail = "ae@gmail.com"
+var accountPassword = 123456
+accountCity = "Jaipur"
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+// console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+

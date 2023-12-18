75accd6b2a6e017ca72d1ffae0a110b9c26ccbc9
rajanshrestha09 <139428256+rajanshrestha09@users.noreply.github.com>
2023-12-18 18:43:02 +0000

    comparision js

---
+++ 01_Basic/04_comparision.js
@@ -0,0 +1,17 @@
+/* comparision operator
+1. >
+2. >=
+3. <
+4. <=
+5. ==
+6. != */
+
+
+//Null
+console.log(null == 0);//false
+console.log(null > 0); //false
+
+
+//Undefined
+console.log(undefined == 0); //false
+console.log(undefined < 0); //false

260168a93c1358c91df0212e45bf1ec01e1576fd
rajanshrestha09 <139428256+rajanshrestha09@users.noreply.github.com>
2023-12-18 18:38:44 +0000

    conversionoperations

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = 'rajan'
+
+console.log(typeof score); //String
+
+let newValue = Number(score)
+
+console.log(newValue); //NaN
+console.log(typeof newValue); //number
+
+let isLoggedIn = Boolean(score)
+console.log(isLoggedIn);//true
+console.log(typeof isLoggedIn); //boolean
+
+let someNumber = 30
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);//30
+console.log(typeof(stringNumber));//string
+
+
+/* operations */
+
+let value =3
+let negValue = -value
+
+console.log(newValue); //Nan
+console.log(typeof newValue);//number
+
+console.log('2'+2); //22 =>string
+
+a = 2 + '2'
+console.log("a", a); //22
+console.log(typeof a); //string

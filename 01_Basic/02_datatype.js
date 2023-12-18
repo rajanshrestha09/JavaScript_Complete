68ad2b288d9a9451b3ace945cc07e9a43a2d3e01
rajanshrestha09 <139428256+rajanshrestha09@users.noreply.github.com>
2023-12-18 18:27:34 +0000

    datatype

---
+++ 01_Basic/02_datatype.js
@@ -0,0 +1,19 @@
+/* Datatypes:
+1. String
+2. Number,
+3. boolean,
+4. null,
+5. undefined
+6. symbol
+7. bigint */
+
+
+// alert(3+3);
+
+let s_name = "hitest" //string
+let age = 18 //number
+let isLoggedIn = false //boolean
+let state; //undefined
+
+console.table([s_name, age, isLoggedIn, state]);
+console.table([typeof s_name, typeof(age), typeof isLoggedIn, typeof state]);
